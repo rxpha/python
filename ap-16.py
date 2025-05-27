1c809cb4ac6a2c135757156f053926e13b7a5190
rxpha <44279227+rxpha@users.noreply.github.com>
2025-05-27 18:42:05 -0400

    corrigindo ex. 4 da ap 4

--- ap-16.py
+++ ap-16.py
@@ -8,8 +8,9 @@ while True:
 
     if len(senha) < 8:
         print("A senha deve ter pelo menos 8 caracteres.")
-    elif not any(char.isdigit() for char in senha):
-        print("A senha deve conter pelo menos um número.")
+    elif (not any(char.isdigit() for char in senha) and 
+          not any(char.isalpha() for char in senha)):
+        print("A senha deve conter pelo menos um dígito e um caractere.")
     else:        
         print("Senha válida.")
         break
